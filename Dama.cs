2f3ce0f261fa5a7309aa053136e84072c4890222
machacekdavi <39218087+machacekdavi@users.noreply.github.com>
2018-07-23 20:52:15 +0200

    Add files via upload

---
+++ Dama.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DamaDavi
+{
+    public class DamaSprava
+    {
+        public static HraciPlocha VytvorLokalniHru(Control panel)
+        {
+            //vytvor 2 lokalni hrace
+            // a pak hraci plochu
+            //je to na testovani
+            throw new NotImplementedException();
+        }
+
+        public static HraciPlocha VytvorSitovouHru(Control panel)
+        {
+            //vytvor 1 lokalni hrace
+            //vytvor 1 sitovy hrac + NET remoting server
+            // a pak hraci plochu
+            throw new NotImplementedException();
+        }
+
+
+        public static HraciPlocha VytvorKlientaSitoveHry(Control panel, string server)
+        {
+            //vytvor 1 lokalni hrace
+            //vytvor 1 sitovy hrac + NET remoting klienta
+            // a pak hraci plochu
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HraciPlocha
+    {
+        
+        public HraciPlocha()
+        {
+
+        }
+
+        //sloupec, radek
+        public HraciPlocha(Control panel, Hrac hrac1, Hrac hrac2)
+        {
+            Panel = panel;
+            Hrac1 = hrac1;
+            Hrac2 = hrac2;
+
+            //ve 2 vnorenych cyklech vyrob vsechny figurky
+            for (int i = 0; i < 8; i++)
+                for (int z = 0; z < 8; z++)
+                {
+                    var figurka = new Figurka[i,z];
+                    // policko.Barva = ((i + z) % 2);  // 0-bily , 1-Cerny
+                    // if (policko.Barva == 1 & i < 3) picture.Image = global::ProgramDama2.Properties.Resources.tmavykamen;
+                    if (((i + z) % 2)==1 & i<3)
+                    {
+                        figurka[i, z].Hrac.Jmeno = "Cerny";
+                    }
+
+                    
+                }
+        }
+
+        public Figurka[,] Sachovnice { get; private set; }
+
+        public Hrac Hrac1 { get; private set; }
+        public Hrac Hrac2 { get; private set; }
+
+        protected Control Panel;
+
+        public void Vykresli()
+        {
+            //v cykle zavola vykresli pre kazdu figurku a vykresli prazdne mista kde Figurka == null
+
+        }
+
+
+        public Figurka NajdiOznacenouFigurku()
+        {
+            //najdi oznacenou figurku, kdyz neni vrat null
+            throw new NotImplementedException();
+        }
+
+        public Figurka NajdiFigurkuPodMysi(int x, int y)
+        {
+            //najdi figurku pod mysi pomoci kalkulace
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Figurka
+    {
+        protected Figurka(HraciPlocha hraciPlocha, Control panel, int sloupec, int radek, Hrac hrac)
+        {
+            HraciPlocha = hraciPlocha;
+            Panel = panel;
+            Sloupec = sloupec;
+            Radek = radek;
+            Hrac = hrac;
+        }
+
+        public int Sloupec { get; private set; }
+
+        public int Radek { get; private set; }
+
+        public Hrac Hrac { get; private set; }
+
+        public bool Oznacena { get; private set; }
+
+        protected HraciPlocha HraciPlocha;
+        protected Control Panel;
+
+        public abstract void Vykresli();
+
+        public abstract bool MuzeTahnout(int sloupec, int radek);
+
+        public void Tahni(int sloupec, int radek)
+        {
+            //presun figurku na pozici pokud to jde, pokud ne vyhod excepsnu
+            //odznac figurku
+            
+            //pokud si vyhodil protihracovu figurku, tak na ni zavolej vyhod a uprav si skore
+            //pokud si v cily, premen za damu
+            //zkontroluj jestli neni konec hry
+            //zavolej Panel.Invalidate na prekresleni
+        }
+
+        public void Vyhod()
+        {
+            //zavolej vyhod figurku na hraci plose
+        }
+
+        public void VymenZaDamu()
+        {
+            //vytvor novou FigurkuDama a vymen ji za tuhle
+        }
+
+        public void OznacFigurku() { }
+    }
+
+    public class FigurkaKamen : Figurka
+    {
+        public FigurkaKamen(HraciPlocha hraciPlocha, Control panel, int sloupec, int radek, Hrac hrac) : base(hraciPlocha, panel, sloupec, radek, hrac)
+        {
+        }
+
+        public override void Vykresli()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool MuzeTahnout(int sloupec, int radek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FigurkaDama : Figurka
+    {
+
+        public FigurkaDama(HraciPlocha hraciPlocha, Control panel, int sloupec, int radek, Hrac hrac) : base(hraciPlocha, panel, sloupec, radek, hrac)
+        {
+        }
+
+        public override void Vykresli()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool MuzeTahnout(int sloupec, int radek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Hrac
+    {
+        protected Hrac(Control panel, string jmeno, HraciPlocha hraciPlocha)
+        {
+            Panel = panel;
+            Jmeno = jmeno;
+            HraciPlocha = hraciPlocha;
+        }
+
+        public string Jmeno { get; set; }
+
+        public bool JeNaTahu { get; set; }
+
+        public int Score { get; set; }
+
+        protected HraciPlocha HraciPlocha;
+
+        protected Control Panel;
+
+        public abstract void Vykresli();
+
+        public virtual void ProtihracTahnul(Figurka figurka, int sloupec, int radek)
+        {
+
+        }
+    }
+
+    public class HracLokalni : Hrac
+    {
+        public HracLokalni(Control panel, string jmeno, HraciPlocha hraciPlocha) : base(panel, jmeno, hraciPlocha)
+        {
+            panel.MouseClick += Panel_MouseClick;
+        }
+
+        private void Panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            //kdyz je na tahu, najdi figurku pod mysi
+            //kdyz neni oznacena figurka, oznac
+            //kdyz je oznacena a da se tahnout, tahni
+            //informuj protihrace ze jsi tahnul
+        }
+
+        public override void Vykresli()
+        {
+            //vykresli hrace a skore
+        }
+    }
+
+    public class HracSitovy : Hrac
+    {
+        public HracSitovy(Control panel, string jmeno, HraciPlocha hraciPlocha) : base(panel, jmeno, hraciPlocha)
+        {
+        }
+
+        public override void Vykresli()
+        {
+            //vykresli hrace a skore
+        }
+
+        public override void ProtihracTahnul(Figurka figurka, int sloupec, int radek)
+        {
+            //HracLokal tahnul, posli spravu na server/klienta 
+
+        }
+
+        public void VzdalenyHracTahnul(int sloupec1, int radek1, int sloupec2, int radek2)
+        {
+            //Obdrzel jsi zpravu ze servru/klienta o tahu
+
+            //najdi figurku na pozici sloupec1, radek1
+            //tahni ji na pozici sloupec2, radek2
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ProgramDama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerujPlochu();  
+        }
+
+        Policko[,] HraciPole = new Policko[8, 8];
+
+        // na zacatku hry plni matici pole, ktera odpovida hraci plose jako prvek pole je pouzito tlacitko,
+        // ktere si nese vsechny atributy o hraci plose k nasledne dalsi manipulaci
+        private void GenerujPlochu()
+        {
+            for (int i = 0; i < 8; i++)
+                for (int z = 0; z < 8; z++)
+                {
+                    var policko = new Policko();
+                    policko.Oznacena = false;
+                    policko.Radek = i;
+                    policko.Sloupec = z;
+                    policko.Barva = ((i + z) % 2);  // 0-bily , 1-Cerny
+                    if (policko.Barva == 1 & i < 3) policko.JeKamen = true;
+                    if (policko.Barva == 1 & i > 4) policko.JeKamen = true;
+                    //policko.Obraz = GenerujObraz(i, z, policko.Barva);
+                    var picture = new PictureBox();
+                    if (policko.Barva == 0) picture.Image = global::ProgramDama2.Properties.Resources.svetly;
+                    else picture.Image = global::ProgramDama2.Properties.Resources.tmavy;
+
+                    if (policko.Barva == 1 & i < 3) picture.Image = global::ProgramDama2.Properties.Resources.tmavykamen;
+                    if (policko.Barva == 1 & i > 4) picture.Image = global::ProgramDama2.Properties.Resources.bilykamen;
+                    picture.Location = new System.Drawing.Point(z * 75, i * 75);
+                    picture.Size = new System.Drawing.Size(75, 75);
+                    picture.TabStop = false;
+                    picture.Click += policko_Click;
+                    HraciPlocha.Controls.Add(picture);
+                    picture.Tag = policko;
+                    policko.Obraz = picture;
+                    HraciPole[i, z] = policko;
+                }
+        }
+
+        // procedura pouzita v GenerujPlocho , slouzi jen pro to aby vygenerovala plochu na zacatku hry,
+        // zakladni rozestaveni obrazku i s kameny
+        private PictureBox GenerujObraz(int i, int z, int barva)
+        {
+            var picture = new PictureBox();
+            if (barva == 0) picture.Image = global::ProgramDama2.Properties.Resources.svetly;
+            else picture.Image = global::ProgramDama2.Properties.Resources.tmavy;
+
+            if (barva == 1 & i < 3) picture.Image = global::ProgramDama2.Properties.Resources.tmavykamen;
+            if (barva == 1 & i > 4) picture.Image = global::ProgramDama2.Properties.Resources.bilykamen;
+            picture.Location = new System.Drawing.Point(z * 75, i * 75);
+            picture.Size = new System.Drawing.Size(75, 75);
+            picture.TabStop = false;
+            picture.Click += policko_Click;
+            HraciPlocha.Controls.Add(picture);
+            return picture;
+        }
+
+        private void policko_Click(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender; 
+            Policko pole = (Policko)picture.Tag;
+
+            if (pole.Barva == 1 & pole.JeKamen == true)
+            {
+                pole.Oznacena = true;
+            }
+
+            string s = "Radek je " + Convert.ToString(pole.Radek)+ " Sloupec je " + Convert.ToString(pole.Sloupec);
+            MessageBox.Show(this, s, "Oznameni", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+
+
+    }
+
+
+
+    public class Policko
+    {
+        public int Radek;
+        public int Sloupec;
+        public PictureBox Obraz;
+        public bool JeKamen;
+        public int Barva; // 0-bily , 1-Cerny
+        public bool Oznacena;
+    }
+
+    class EchoServer
+    {
+        Socket s;
+
+        public bool StartUp(IPAddress ip, int port)
+        {
+            try
+            {
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                    ProtocolType.Tcp);
+                s.Bind(new IPEndPoint(ip, port));
+                s.Listen(10);  // maximal 10 clients in queue
+            }
+            catch (Exception e) { Console.WriteLine("An error occurred: '{0}'", e); }
+            for (;;)
+            {
+                Communicate(s.Accept());  // waits for connecting clients
+            }
+        }
+        // returns all the received data back to the client
+        public void Communicate(Socket clSock)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                while (clSock.Receive(buffer) > 0)  // receive data
+                    clSock.Send(buffer);  // send back the data
+                clSock.Shutdown(SocketShutdown.Both);  // close sockets
+                clSock.Close();
+            }
+            catch (Exception e) { Console.WriteLine("An error occurred: '{0}'", e); }
+        }
+    }
+
+
+    class EchoClient
+    {
+
+        public static void Hlavni()
+        {
+            try
+            {
+                // connect to the server
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(new IPEndPoint(IPAddress.Loopback, 5000));
+                s.Send(Encoding.ASCII.GetBytes("This is a test"));  // send the message
+
+                byte[] echo = new byte[1024];
+                s.Receive(echo);  // receive the echo message
+                Console.WriteLine(Encoding.ASCII.GetString(echo));
+            }
+            catch (Exception e) { Console.WriteLine("An error occurred: '{0}'", e); }
+        }
+    }
+}
+
